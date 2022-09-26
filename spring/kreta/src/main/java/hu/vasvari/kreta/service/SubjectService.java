f0d66f55c6c5db2ed7d4b5888186be8d356230d6
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-26 12:46:54 +0200

    Subject és repo réteg kapcsolat

--- spring/kreta/src/main/java/hu/vasvari/kreta/service/SubjectService.java
+++ spring/kreta/src/main/java/hu/vasvari/kreta/service/SubjectService.java
@@ -1,4 +1,12 @@
 package hu.vasvari.kreta.service;
 
+import hu.vasvari.kreta.repo.interfaces.ISubjectRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SubjectService {
+
+    @Autowired
+    ISubjectRepo repo;
 }
