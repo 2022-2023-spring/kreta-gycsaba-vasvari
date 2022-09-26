7d00d2fee09bcedf77da481d9053f0f2ecc724af
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-26 12:55:44 +0200

    ISubjectService

--- spring/kreta/src/main/java/hu/vasvari/kreta/service/SubjectService.java
+++ spring/kreta/src/main/java/hu/vasvari/kreta/service/SubjectService.java
@@ -1,12 +1,35 @@
 package hu.vasvari.kreta.service;
 
+import hu.vasvari.kreta.model.Subject;
 import hu.vasvari.kreta.repo.interfaces.ISubjectRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class SubjectService {
+public class SubjectService implements ISubjectService {
 
     @Autowired
     ISubjectRepo repo;
+
+    @Override
+    public List<Subject> getAllSubject() {
+        return null;
+    }
+
+    @Override
+    public Subject getSubjectById(long id) {
+        return null;
+    }
+
+    @Override
+    public long saveOrUpdate(Subject subject) {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(long id) {
+
+    }
 }
