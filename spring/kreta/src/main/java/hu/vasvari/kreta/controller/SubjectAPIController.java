a59098666896fb7b0ca2f1df4885940b3bdc86e7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-30 10:33:09 +0200

    Mappingok

--- spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectAPIController.java
+++ spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectAPIController.java
@@ -3,15 +3,19 @@ package hu.vasvari.kreta.controller;
 import hu.vasvari.kreta.model.Subject;
 import hu.vasvari.kreta.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class SubjectController {
+@RequestMapping(value = "/api", name = "Subject API controller")
+public class SubjectAPIController {
     @Autowired
     SubjectService service;
 
+    @GetMapping(value = "/subject",name = "Get all subjects")
     public List<Subject> getAllSubject() {
         return service.getAllSubject();
     }
