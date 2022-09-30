e66c2c2771dfca4d7a3601cfb3d89a059e566dc0
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-30 10:25:11 +0200

    Kapcsolat a service réteggel

--- spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectController.java
+++ spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectController.java
@@ -1,7 +1,11 @@
 package hu.vasvari.kreta.controller;
 
+import hu.vasvari.kreta.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SubjectController {
+    @Autowired
+    SubjectService service;
 }
