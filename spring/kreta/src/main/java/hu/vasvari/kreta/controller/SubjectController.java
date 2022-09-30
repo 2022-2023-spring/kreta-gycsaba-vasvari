265b5eb2b6ede750db011fc743a98ce772073120
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-30 10:26:39 +0200

    getAllSubject metódus

--- spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectController.java
+++ spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectController.java
@@ -1,11 +1,19 @@
 package hu.vasvari.kreta.controller;
 
+import hu.vasvari.kreta.model.Subject;
 import hu.vasvari.kreta.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SubjectController {
     @Autowired
     SubjectService service;
+
+    public List<Subject> getAllSubject() {
+        return service.getAllSubject();
+    }
+
 }
