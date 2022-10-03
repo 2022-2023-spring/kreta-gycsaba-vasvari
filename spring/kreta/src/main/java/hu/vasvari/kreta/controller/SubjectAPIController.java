209ecd2515d3633b532ee6a5f301ec6a85b75d1a
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-10-03 13:24:28 +0200

    elérések

--- spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectAPIController.java
+++ spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectAPIController.java
@@ -13,21 +13,25 @@ public class SubjectAPIController {
     @Autowired
     SubjectService service;
 
+    //localhost:8888/api/subject/
     @GetMapping(value = "/subject",name = "Get all subjects")
     public List<Subject> getAllSubject() {
         return service.getAllSubject();
     }
 
+    //localhost:8888/api/subject/
     @PostMapping(value = "/subject", name="Save subject")
     public void saveSubject(@RequestBody Subject subject) {
         service.saveOrUpdate(subject);
     }
 
+    //localhost:8888/api/subject/
     @PutMapping(value="/subject",name="Update subject")
     public void updateSubject(@RequestBody Subject subject) {
         service.saveOrUpdate(subject);
     }
 
+    //localhost:8888/api/subject/1
     @DeleteMapping(value = "/subject/{id}",name="Delete subject")
     public void deleteSubject(@PathVariable long id) {
         service.deleteById(id);
