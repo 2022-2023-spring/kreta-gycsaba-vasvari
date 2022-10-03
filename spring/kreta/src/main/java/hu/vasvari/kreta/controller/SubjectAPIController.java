4115d7904967752a053e3a39d5d47052254aa7e0
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-10-03 13:21:42 +0200

    deleteSubject

--- spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectAPIController.java
+++ spring/kreta/src/main/java/hu/vasvari/kreta/controller/SubjectAPIController.java
@@ -3,9 +3,7 @@ package hu.vasvari.kreta.controller;
 import hu.vasvari.kreta.model.Subject;
 import hu.vasvari.kreta.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +18,18 @@ public class SubjectAPIController {
         return service.getAllSubject();
     }
 
+    @PostMapping(value = "/subject", name="Save subject")
+    public void saveSubject(@RequestBody Subject subject) {
+        service.saveOrUpdate(subject);
+    }
+
+    @PutMapping(value="/subject",name="Update subject")
+    public void updateSubject(@RequestBody Subject subject) {
+        service.saveOrUpdate(subject);
+    }
+
+    @DeleteMapping(value = "/subject/{id}",name="Delete subject")
+    public void deleteSubject(@PathVariable long id) {
+        service.deleteById(id);
+    }
 }
